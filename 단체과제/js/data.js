4192aeecc4d348c2ec9b691c039a3208f42c4930
magnoria <sidaumv@gmail.com>
2024-12-18 00:33:20 +0900

    백업

--- 단체과제/js/data.js
+++ 단체과제/js/data.js
@@ -47,3 +47,44 @@ function getBoard(bno){
 
      return null; //조회할 게시물번호와 일치한 게시물 객체 못찾았을때는 null
 }
+
+function delitPut( bno ){ // 매개변수 , 삭제할 인덱스 번호 
+     // console.log( '삭제함수 실행' );
+      //console.log( index )
+      //게시물목록.splice( index , 1 ); 
+     // 출력함수();
+  
+      // 1. 삭제할 bno의 인덱스를 찾기
+     let boardList = getBoardList();
+      //2. 게시물 목록에서 삭제할 번호의 인덱스 찾는것것
+      let deleteIndex = -1;
+      for( let index = 0; index <= boardList.length-1; index++){
+          if(boardList[index].bno == bno){
+              deleteIndex = index;
+              break;
+          }
+      }
+      //3. 삭제할 번호의 게시물 인덱스를 게시물 목록에서 삭제제
+      boardList.splice(deleteIndex,1);
+      //4. localStorage 업데이트
+      setBoardList(boardList);
+      //5. 알림
+      alert("삭제완료.");
+      location.href="board.html"
+  
+  }// f end
+
+  function corRection(bno){
+
+     let boadList = getBoardList();
+
+     for(let index = 0; index <= boadList.length-1; index++){
+          if(boadList[index].bno == bno){
+               if(boadList[index].password == ) //  변경함수의 password 변수를 view에서 받을지 고민중
+          }
+     }// for end
+
+     
+
+
+  }//f end
